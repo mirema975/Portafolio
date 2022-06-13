7e71f6f6e934f4f9f87a5bf0fd334692733a782d
Emanuel Miranda <emanuelmiranda975@gmail.com>
2022-06-13 12:06:58 -0300

    se ajustó el diseño del portafolio, ajustando las columnas de forma responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,45 @@
+window.onload = function() {
+    getCard();
+}
+
 function saltarA(id, tiempo) {
     var tiempo = tiempo || 500;
     $("html, body").animate({ scrollTop: $(id).offset().top }, tiempo);
+}
+
+async function getCard() {
+
+    const rawResponse = await fetch('http://127.0.0.1:8080/api/proyecto', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    });
+
+    const proyectos = await rawResponse.json();
+
+    let HTMLParaAgregar = ''
+
+    for (pro of proyectos) {
+
+        HTMLParaAgregar  += `
+        <div class="card">
+        <div class="card__img">
+        </div>
+        <div class="card__title">
+        <h1>${pro.nombre}</h1>
+        <span>${pro.descripcion}</span>
+        </div>
+        <div class="card__projects-links">
+        <a href="${pro.repositorio}" target="_blank">
+        <i class="fab fa-github"></i><br> <br>
+        <span>Repositorio</span>
+        </a>
+        </div>
+        </div>`;
+    }
+    
+    document.getElementById('portafolio-container').innerHTML = HTMLParaAgregar;
+
 }

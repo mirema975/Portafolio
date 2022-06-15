3a622199625bb90fec6d312881ed2fd4ddb9d345
Emanuel Miranda <emanuelmiranda975@gmail.com>
2022-06-15 11:37:59 -0300

    se quito modo no-cors en la funcion getCard

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,6 @@ function saltarA(id, tiempo) {
 async function getCard() {
 
     const rawResponse = await fetch('https://api.emamir.site:8443/api/proyecto', {
-        mode: 'no-cors',
         method: 'GET',
         headers: {
             'Accept': 'application/json',
